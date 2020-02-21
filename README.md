d42e5e9d076f35b68d0bda5f379f9e0ba03212b3
jyh7a <jyh7a@naver.com>
2020-02-22 00:47:32 +0900

    readme update

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 카카오 클론코딩 졸업 작품: https://jyh7a.github.io/NomadCoders/kakao_instar/
 
 바닐라 자바스트립트 졸업 작품: https://jyh7a.github.io/NomadCoders/Vanilla_Javascript_1/3_Make_your_first_JS_App/
+
+# Fonts
+Fira Code
--- wetube/index.js
+++ wetube/index.js
@@ -1,1 +1,22 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+function handleHome(req, res) {
+  res.send("HI from home!");
+}
+
+function handleProfile(req, res) {
+  res.send("You are In Profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
