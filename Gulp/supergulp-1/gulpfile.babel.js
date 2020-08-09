19264844b68d531598d35409ac8c5795fa03ef98
jyh7a <jyh7a@naver.com>
2020-08-10 04:00:17 +0900

    gulp

---
+++ Gulp/supergulp-1/gulpfile.babel.js
@@ -0,0 +1,36 @@
+import gulp from "gulp"
+import gpug from 'gulp-pug'
+import del from "del"
+import ws from "gulp-webserver"
+
+const routes = {
+  
+  pug: {
+    watch: "src/**/*.pug",
+    src: "src/*.pug",
+    dest: "build"
+  }
+
+}
+
+
+const pug = () => gulp.src(routes.pug.src)
+.pipe(gpug())
+.pipe(gulp.dest(routes.pug.dest))
+
+const clean = () => del(["build/"])
+
+const webserver = () => gulp.src("build")
+.pipe(ws({livereload: true, open: true}))
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug)
+}
+
+const prepare = gulp.series([clean])
+
+const assets = gulp.series([pug])
+
+const postDev = gulp.parallel([webserver, watch])
+
+export const dev = gulp.series([prepare, assets, postDev])
